fb42d8d7e1782d04e432ce39ddbad16ccdfebd3c
servomouse <servomouse@protonmail.com>
2025-05-15 22:48:59 -0600

    Add the dictionary

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+
+durations = {
+    "dot": 100,
+    "dash": durations['dot'] * 3,
+    "inletter": durations['dot'],
+    "inword": durations['dot'] * 3,
+    "word_sep": durations['dot'] * 7
+}
+
+symbols = {
+    "a": [],
+}
